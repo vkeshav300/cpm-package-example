afa7564330c78c9dabac9bdf7aa9d5e680599e30
Keshav Verma <vkeshav300@gmail.com>
2024-01-28 16:01:26 -0800

    Initial commit

---
+++ package-name/file-name.cpp
@@ -0,0 +1,12 @@
+#include "file-name.h"
+
+#include <iostream>
+
+namespace example
+{
+	int foo()
+	{
+		std::cout << "Hello World!" << std::endl;
+		return 0;
+	}
+}
---
+++ package-name/file-name.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace example
+{
+	int foo();
+}
